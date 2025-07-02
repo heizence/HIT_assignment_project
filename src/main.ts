84246cda0a18b741c065a75a39b8f825b6eb9a30
doheon <heizence6626@gmail.com>
2025-07-02 21:38:17 +0900

    feat(main) : Add new configurations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 전역 유효성 검사 파이프 추가
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 속성은 자동으로 제거
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 속성이 들어오면 에러 발생
+      transform: true, // 들어오는 데이터를 DTO 클래스 타입으로 변환
+    }),
+  );
+
+  await app.listen(3000);
 }
 bootstrap();
