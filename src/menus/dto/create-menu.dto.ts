f000a05b21e30e67abe0edf8ccad26eed760a888
doheon <heizence6626@gmail.com>
2025-07-03 19:09:35 +0900

    fix(create-menu.dto) : Add missing ApiProperty

--- src/menus/dto/create-menu.dto.ts
+++ src/menus/dto/create-menu.dto.ts
@@ -5,7 +5,10 @@ import { IsString, IsNumber, IsIn, Min, IsOptional } from 'class-validator';
 const validCategories = ['한식', '중식', '일식', '양식', '기타'];
 
 export class CreateMenuDto {
-  @ApiProperty({ example: '오늘의 파스타', description: '메뉴 이름' })
+  @ApiProperty({
+    example: '오늘의 파스타',
+    description: '메뉴 이름',
+  })
   @IsString()
   name: string;
 
@@ -14,9 +17,14 @@ export class CreateMenuDto {
   @Min(0)
   price: number;
 
+  @ApiProperty({ example: '양식', description: '카테고리' })
   @IsIn(validCategories)
   category: string;
 
+  @ApiProperty({
+    example: '메뉴 설명(optional)',
+    description: '메뉴에 대한 설명',
+  })
   @IsString()
   @IsOptional()
   description?: string;
